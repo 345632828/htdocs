945cfdd8ece6af3086bf7d53164e8fcefdb88446
chenjun <75957331@qq.com>
2018-02-24 10:27:01 +0800

    add test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ http.createServer(function (request, response) {
 	// HTTP 状态值: 200 : OK
 	// 内容类型: text/plain
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-
+	// add test
 	// 发送响应数据 "Hello World"
 	response.end('Hellos Worlds\n');
 }).listen(8888);
